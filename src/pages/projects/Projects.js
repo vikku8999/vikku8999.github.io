9454acd6f6ef536b1229682d0d11a59d2c7af94d
vikku8999 <76919748+vikku8999@users.noreply.github.com>
2021-09-06 13:09:25 +0530

    seventh commit

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -53,7 +53,7 @@ class Projects extends Component {
         <Button
           text={"More Projects"}
           className="project-button"
-          href="https://github.com/ashutosh1919"
+          href="https://github.com/vikku8999"
           newTab={true}
           theme={theme}
         />
